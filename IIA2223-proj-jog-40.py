6f38d47729dc0ff4896c370a07a87a2398028637
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-11-12 16:06:41 +0000

    Add JogoBT class skeleton.

--- IIA2223-proj-jog-40.py
+++ IIA2223-proj-jog-40.py
@@ -3,3 +3,20 @@
 #  54685 - Francisco Correia
 #  55855 - Francisco Maia
 #  55955 - Alexandre Fonseca
+
+from jogos import Game
+
+
+class JogoBT_40(Game):
+    def __init__(self):
+        # self.initial = EstadoBT_40()
+        pass
+
+    def actions(self, state):
+        return super().actions(state)
+
+    def result(self, state, move):
+        return super().result(state, move)
+
+    def utility(self, state, player):
+        return super().utility(state, player)
