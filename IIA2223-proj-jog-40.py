10694d49cadfabe47b96c3529c01f30c62d54ae9
XicoCorreia <xicocorreia17@gmail.com>
2022-11-21 23:21:38 +0000

    Jogador Heuristico nao 100% funcional

--- IIA2223-proj-jog-40.py
+++ IIA2223-proj-jog-40.py
@@ -6,6 +6,7 @@
 #  55955 - Alexandre Fonseca
 
 from functools import reduce
+import collections
 from jogos import (
     Game,
     GameState,
@@ -133,13 +134,70 @@ class JogadorAlfaBetaAlt(Jogador):  # faz só utility()
 
     def display(self):
         print(self.nome + " ")
+        
+def f_aval_jogadorheuristico(estado: EstadoBT_40, jogador):
+    player = "W" if jogador == 1 else "B"
+    pieces = [(col,row) for (col, row), val in estado.board.items() if val == player]
+    piecesOtherCols = [col for (col, _), val in estado.board.items() if val != player]
+    piecesCols = [col for (col, _), val in estado.board.items() if val == player]
+    res = 0
+    if jogador == 1:
+        for (j,i) in pieces:   
+            #Brancas ganharam
+            if i == 8:
+                return 100000
+            #Peso da linha
+            res += i**2
+            #Verificar se é possivel ganhar na prox jogada
+            if i == 7:
+                if threat(estado, jogador, (j,i), 1): 
+                    res += 10000
+            #Numero de peças iguais na mesma coluna, ou diferentes
+            res -= piecesCols.count(j)
+            res += piecesOtherCols.count(j)
 
+            
+            
+    else:
+        for (j,i) in pieces:
+            if i == 1:
+                return 100000
+            i = 9 - i
+            res += i**2
+            res -= piecesCols.count(j)
+            res += piecesOtherCols.count(j)
+            if i == 2:
+                if threat(estado, jogador, (j,i), -1): 
+                    res += 10000
+            res -= piecesCols.count(j)
+            res += piecesOtherCols.count(j)
+            
+    
+    return res
 
+def threat(estado: EstadoBT_40, jogador, tup, diff):
+    player = "W" if jogador == 1 else "B"
+    col, row = tup
+    pieces = [(col,row) for (col, row), val in estado.board.items() if val != player]
+    dict ={'a':1, 'b':2, 'c':3 , 'd':4 , 'e':5 , 'f':6 ,'g':7, 'h':8 }
+    n = dict.get(col)
+    if n > 1:
+        for (c, r) in pieces:
+            if r == row + 1 and dict.get(c) == n - diff:
+                return False
+    if n < 8:
+        for (c, r) in pieces:
+            if r == row + 1 and dict.get(c) == n + diff:
+                return False
+
+    return True
+    
 jogo = JogoBT_40()
-j1 = JogadorAlfaBeta("Belarmino", 1, f_aval_belarmino)  # atualmente depth = 1
-j2 = Jogador("Random 1", random_player)
-j3 = Jogador("Random 2", random_player)
-j4 = Jogador("Random 3", random_player)
+j1 = JogadorAlfaBeta("Belarmino", 2, f_aval_belarmino)  # atualmente depth = 1
+j2 = JogadorAlfaBeta("JogadorHeuristico", 2, f_aval_jogadorheuristico) 
+#j3 = Jogador("Random 1", random_player)
+#j4 = Jogador("Random 2", random_player)
+#j5 = Jogador("Random 3", random_player)
 # j5 = JogadorAlfaBetaAlt("Alfabeta")
 # j6 = Jogador("NÓS", query_player)
-faz_campeonato(jogo, [j1, j2, j3, j4], 10)
+faz_campeonato(jogo, [j1, j2], 10)
